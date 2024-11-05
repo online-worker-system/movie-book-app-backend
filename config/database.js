4d0ef1378ac077ea75930cf3bd0ccaf9f0c6299e
Shivanshu-Bhawsar <shivanshubhawsar3@gmail.com>
2024-11-05 18:33:04 +0530

    server update

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-require("dotenv").config();
-
-exports.dbconnect= ()=>{
+const dbConnect = () => {
     mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>{console.log("DB Connected Successfully")})
-    .catch((error)=>{
-        console.log("DB Connection Failed");
-        console.log(error);
+    .then(() => {
+        console.log("Database connection established");
+    })
+    .catch((error) => {
+        console.log("DB Connection Failed: ", error.message);
         process.exit(1);
     })
-};
+}
+
+module.exports = dbConnect;
--- server.js
+++ server.js
@@ -1,6 +1,42 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const dbConnect = require("./config/database");
+// const { upload, imageUploadUtil } = require("./helpers/cloudinary");
 
-app.listen(3000,()=>{
-    console.log("server is started");
-})
+const authRouter = require("./routes/auth/auth-routes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+dbConnect();
+
+// middleware setup
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+// // Add route to handle image upload using Cloudinary
+// app.post("/api/upload", upload.single("file"), async (req, res) => {
+//   try {
+//     const result = await imageUploadUtil(req.file);
+//     res.json({ message: "Upload successful", url: result.secure_url });
+//   } catch (error) {
+//     res.status(500).json({ error: "Upload failed", details: error.message });
+//   }
+// });
+
+// route handlers
+app.use("/api/auth", authRouter);
+
+// start server
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+
+// default route
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "Your server is up and running....",
+  });
+});
